6e04a46164c4fc80880a3c7a1a521ddab5b79cde
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-10-25 01:24:17 +0300

    first 2

---
+++ BonusScore/BonusScore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class BonusScore
+    {
+        static void Main(string[] args)
+        {
+            int score = int.Parse(Console.ReadLine());
+            if (score > 0 && score <= 3)
+            {
+                score = score * 10;
+                Console.WriteLine(score);
+            }
+            else if (score > 3 && score <= 6)
+            {
+                score = score * 100;
+                Console.WriteLine(score);
+            }
+            else if (score > 6 && score <= 9)
+            {
+                score = score * 1000;
+                Console.WriteLine(score);
+            }
+            //else if (score == 0)
+            //{
+            //    Console.WriteLine("0");
+            //}
+            else
+            {
+                Console.WriteLine("invalid score");
+            }
+        }
+    }
+}
---
+++ ExchangeNumbers/ExchangeNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExchangeNumbers
+{
+    class ExchangeNumbers
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double c = new double();
+
+            if (a < b)
+            {
+                Console.WriteLine(a + " " + b);
+            }
+            else
+            {
+                c = a;
+                a = b;
+                b = c;
+                Console.WriteLine(a + " " + b);
+            }
+        }
+    }
+}
